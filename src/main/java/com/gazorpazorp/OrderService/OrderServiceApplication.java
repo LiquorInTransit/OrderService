cb46097c18b95f96bff68c0dcd45ba2a2ea71ad4
Christopher Peckover <christopher.peckover@gmail.com>
2017-09-12 13:50:47 -0400

    AWS and Profile Quick Fix
    -added AWS eureka info and for !dev profile

--- src/main/java/com/gazorpazorp/OrderService/OrderServiceApplication.java
+++ src/main/java/com/gazorpazorp/OrderService/OrderServiceApplication.java
@@ -3,9 +3,12 @@ package com.gazorpazorp.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -13,6 +16,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import com.gazorpazorp.client.config.CustomOAuth2FeignRequestInterceptor;
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication(scanBasePackages="com.gazorpazorp")
 //@ComponentScan(basePackages="com.gazorpazorp", excludeFilters = @Filter(type = FilterType.REGEX, pattern = "com.gazorpazorp.clientConfig.*"))
@@ -57,4 +61,17 @@ public class OrderServiceApplication {
 //	        template.setAccessTokenProvider(accessTokenProviderChain);
 //	    };
 //	}
+	
+	@Bean
+	@Profile("!dev")
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+		EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		instance.setDataCenterInfo(info);
+		instance.setNonSecurePort(8080);
+		return instance;
+	}
 }
