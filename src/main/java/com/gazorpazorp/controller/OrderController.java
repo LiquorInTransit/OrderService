2b96e425c3e4c0e60fdda86d54c1e0c518e70b01
Christopher Peckover <christopher.peckover@gmail.com>
2017-09-15 17:26:50 -0400

    Add /api to endpoint

--- src/main/java/com/gazorpazorp/controller/OrderController.java
+++ src/main/java/com/gazorpazorp/controller/OrderController.java
@@ -24,7 +24,7 @@ import com.gazorpazorp.repository.OrderRepository;
 import com.gazorpazorp.service.OrderService;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/orders")
 public class OrderController {
 	
 	@Autowired
